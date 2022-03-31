d8d7898ba61da97aea36b5af21011c5a504b136a
a5r0n <a5r0n@users.noreply.github.com>
2022-04-01 01:52:58 +0300

    test: Add some tests

--- tests/test_whatsapp.py
+++ tests/test_whatsapp.py
@@ -1,5 +1,8 @@
+import asyncio
 import mimetypes
 from typing import List, Tuple
+
+import aiohttp
 from whatsapp import __version__
 import pytest
 from whatsapp import WhatsAppClient, WhatsAppConfig
@@ -7,82 +10,159 @@ from whatsapp import WhatsAppClient, WhatsAppConfig
 import os
 
 
-def test_version():
-    assert __version__ == "0.1.3"
+def test_whatsapp_config(whatsapp_config):
+    assert whatsapp_config.use_token == False
+    assert whatsapp_config.wa_id != ""
+    assert whatsapp_config.user_agent != ""
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    return asyncio.get_event_loop()
+
+
+@pytest.fixture(scope="session")
+def config_path() -> str:
+    return os.path.join(os.path.dirname(__file__), "..", ".config.yaml")
+
+
+@pytest.fixture(scope="session")
+def whatsapp_config(config_path):
+    return WhatsAppConfig(config_path)
 
 
 @pytest.fixture
-async def client():
-    wa_id = os.getenv("WHATSAPP_ID")
-    endpoint = os.getenv("WHATSAPP_ENDPOINT")
-    client = WhatsAppClient(WhatsAppConfig(wa_id=wa_id, endpoint=endpoint))
+def logged_out_whatsapp_config(config_path):
+    c = WhatsAppConfig(config_path)
+    c.use_token = True
+    return c
+
+
+@pytest.fixture
+async def logged_out_client(logged_out_whatsapp_config):
+    client = WhatsAppClient(logged_out_whatsapp_config)
     async with client:
         yield client
 
 
-@pytest.fixture
-def to():
-    return os.getenv("WHATSAPP_TO")
+@pytest.fixture(scope="session")
+async def client(whatsapp_config):
+    client = WhatsAppClient(whatsapp_config)
+    async with client:
+        yield client
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def to(whatsapp_config):
+    return os.getenv("WHATSAPP_TO", whatsapp_config.wa_id)
+
+
+@pytest.fixture(scope="session")
 def text():
     return "Hello World!"
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def buttons():
     return [("1", "Button 1"), ("2", "Button 2")]
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def list_title():
     return "List Title"
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def list_buttons():
     return [("1", "Button 1"), ("2", "Button 2")]
 
 
-@pytest.fixture
-def image_path():
-    return os.path.join(os.path.dirname(__file__), "image.jpg")
+@pytest.fixture(scope="session")
+def base_path():
+    return os.path.join(os.path.dirname(__file__), "files")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def image_path(base_path):
+    return os.path.join(base_path, "image.jpg")
+
+
+@pytest.fixture(scope="session")
 def image_url():
-    return "https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"
+    return "https://filesamples.com/samples/image/jpeg/sample_640%C3%97426.jpeg"
 
 
-@pytest.fixture
-def document_path():
-    return os.path.join(os.path.dirname(__file__), "test.pdf")
+@pytest.fixture(scope="session")
+def document_path(base_path):
+    return os.path.join(base_path, "test.pdf")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def document_url():
-    return "https://s2.q4cdn.com/498544986/files/doc_downloads/test.pdf"
+    return "https://filesamples.com/samples/document/pdf/sample1.pdf"
 
 
-@pytest.fixture
-def audio_path():
-    return os.path.join(os.path.dirname(__file__), "audio.mp3")
+@pytest.fixture(scope="session")
+def audio_path(base_path):
+    return os.path.join(base_path, "audio.mp3")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def audio_url():
-    return "https://file-examples.com/storage/fe07f859fd624073f9dbdc6/2017/11/file_example_MP3_700KB.mp3"
+    return "https://filesamples.com/samples/audio/mp3/sample1.mp3"
 
 
-@pytest.fixture
-def video_path():
-    return os.path.join(os.path.dirname(__file__), "video.mp4")
+@pytest.fixture(scope="session")
+def video_path(base_path):
+    return os.path.join(base_path, "video.mp4")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def video_url():
-    return "https://file-examples.com/storage/fe07f859fd624073f9dbdc6/2017/04/file_example_MP4_480_1_5MG.mp4"
+    return "https://filesamples.com/samples/video/mp4/sample_640x360.mp4"
+
+
+@pytest.fixture(scope="module")
+async def image_media(client: WhatsAppClient, image_path: str):
+    return await upload_media(client, image_path)
+
+
+@pytest.fixture(scope="session")
+async def document_media(client: WhatsAppClient, document_path: str):
+    return await upload_media(client, document_path)
+
+
+@pytest.fixture(scope="session")
+async def audio_media(client: WhatsAppClient, audio_path: str):
+    return await upload_media(client, audio_path)
+
+
+@pytest.fixture(scope="session")
+async def video_media(client: WhatsAppClient, video_path: str):
+    return await upload_media(client, video_path)
+
+
+@pytest.fixture
+async def aiohttp_client():
+    async with aiohttp.ClientSession() as session:
+        yield session
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "file_url", ["image_url", "video_url", "audio_url", "document_url"]
+)
+async def test_urls_are_valid(file_url, aiohttp_client, request):
+    url = request.getfixturevalue(file_url)
+    async with aiohttp_client.head(url) as response:
+        assert response.status == 200
+
+
+@pytest.mark.asyncio
+async def test_login(logged_out_client: WhatsAppClient):
+    assert logged_out_client.config.token is None
+    assert not logged_out_client.config.is_logged_in
+    assert await logged_out_client.login()
 
 
 @pytest.mark.asyncio
@@ -109,9 +189,17 @@ async def test_send_list(
 
 
 @pytest.mark.asyncio
-async def test_upload_media(client: WhatsAppClient, image_path: str):
-    with open(image_path, "rb") as f:
-        media_resp = await client.upload(f.read(), mimetypes.guess_type(image_path)[0])
+@pytest.mark.parametrize(
+    "file_path", ["image_path", "video_path", "audio_path", "document_path"]
+)
+async def test_upload_media(client: WhatsAppClient, file_path: str, request):
+    file_path = request.getfixturevalue(file_path)
+    media_resp = await upload_media(client, file_path)
+
+
+async def upload_media(client: WhatsAppClient, file_path: str):
+    with open(file_path, "rb") as f:
+        media_resp = await client.upload(f.read(), mimetypes.guess_type(file_path)[0])
 
     media_id = media_resp.media.pop().id
     assert media_id is not None
@@ -119,34 +207,43 @@ async def test_upload_media(client: WhatsAppClient, image_path: str):
 
 
 @pytest.mark.asyncio
-async def test_send_image_local(client: WhatsAppClient, to: str, image_path: str):
-    media_id = await test_upload_media(client, image_path)
-    await client.send_image(to, media_id=media_id)
+@pytest.mark.parametrize("caption", ["With Caption", "", None, "With\nNew\nLines"])
+async def test_send_image_local(
+    client: WhatsAppClient, to: str, image_media: str, caption, request
+):
+    await client.send_image(to, media_id=image_media, caption=caption)
 
 
 @pytest.mark.asyncio
-async def test_send_image_url(client: WhatsAppClient, to: str, image_url: str):
-    resp = await client.send_image(to, media_link=image_url)
+async def test_send_image_url(client: WhatsAppClient, to: str, image_url: str, caption):
+    resp = await client.send_image(to, media_link=image_url, caption=caption)
 
 
 @pytest.mark.asyncio
-async def test_send_document_local(client: WhatsAppClient, to: str, document_path: str):
-    media_id = await test_upload_media(client, document_path)
+@pytest.mark.parametrize(
+    "filename", ["With Filename.pdf", "", None, "With\nNew\nLines.pdf"]
+)
+async def test_send_document_local(
+    client: WhatsAppClient, to: str, document_media: str, filename
+):
+
     await client.send_document(
-        to, media_id=media_id, filename="test.pdf", caption="Test"
+        to, media_id=document_media, filename=filename, caption="Test"
     )
 
 
 @pytest.mark.asyncio
-async def test_send_document_url(client: WhatsAppClient, to: str, document_url: str):
+async def test_send_document_url(
+    client: WhatsAppClient, to: str, document_url: str, filename
+):
     resp = await client.send_document(
-        to, media_link=document_url, filename="test.pdf", caption="Test"
+        to, media_link=document_url, filename=filename, caption="Test"
     )
 
 
 @pytest.mark.asyncio
 async def test_send_audio_local(client: WhatsAppClient, to: str, audio_path: str):
-    media_id = await test_upload_media(client, audio_path)
+    media_id = await upload_media(client, audio_path)
     await client.send_audio(to, media_id=media_id)
 
 
@@ -156,11 +253,15 @@ async def test_send_audio_url(client: WhatsAppClient, to: str, audio_url: str):
 
 
 @pytest.mark.asyncio
-async def test_send_video_local(client: WhatsAppClient, to: str, video_path: str):
-    media_id = await test_upload_media(client, video_path)
-    await client.send_video(to, media_id=media_id)
+@pytest.mark.parametrize("caption", ["With Caption", "", None, "With\nNew\nLines"])
+async def test_send_video_local(
+    client: WhatsAppClient, to: str, video_media: str, caption: str
+):
+    await client.send_video(to, media_id=video_media, caption=caption)
 
 
 @pytest.mark.asyncio
-async def test_send_video_url(client: WhatsAppClient, to: str, video_url: str):
-    resp = await client.send_video(to, media_link=video_url)
+async def test_send_video_url(
+    client: WhatsAppClient, to: str, video_url: str, caption: str
+):
+    resp = await client.send_video(to, media_link=video_url, caption=caption)
